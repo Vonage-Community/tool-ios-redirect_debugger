074ccfa0297f4a12eaab35f542a51516e31ab017
Chris Tankersley <chris@ctankersley.com>
2025-11-10 10:48:52 -0500

    Initial Commit

---
+++ tool-ios-redirect_debugger/tool_ios_redirect_debuggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  tool_ios_redirect_debuggerApp.swift
+//  tool-ios-redirect_debugger
+//
+//  Created by Chris Tankersley on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct tool_ios_redirect_debuggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tool-ios-redirect_debuggerTests/tool_ios_redirect_debuggerTests.swift
@@ -0,0 +1,17 @@
+//
+//  tool_ios_redirect_debuggerTests.swift
+//  tool-ios-redirect_debuggerTests
+//
+//  Created by Chris Tankersley on 11/10/25.
+//
+
+import Testing
+@testable import tool_ios_redirect_debugger
+
+struct tool_ios_redirect_debuggerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
